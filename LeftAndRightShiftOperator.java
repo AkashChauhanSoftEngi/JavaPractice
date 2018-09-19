abd502ae607f595821217b980a2f9cc6c5a5a474
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-19 14:20:16 +0530

    Add files via upload

--- LeftAndRightShiftOperator.java
+++ LeftAndRightShiftOperator.java
@@ -4,6 +4,7 @@ package com.project.java;
  * Left Shift	: N*(2^b)
  * Right Shift	: N/(2^b)
  * It preserves the sign bit [Left most]
+ * Reference: https://www.youtube.com/watch?v=cymclYDL_Ec
  * */
 
 public class LeftAndRightShiftOperator {
