f86b0f95225d326cccc2404a72958352273082c6
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-19 14:35:36 +0530

    Add files via upload

--- UnsignedRightShiftOperator.java
+++ UnsignedRightShiftOperator.java
@@ -5,12 +5,18 @@ package com.project.java;
  * Why no unsigned left Operator[<<<]?
  * - As <<, serves the same purpose.
  * It does not preserve the sign bit [Left most]
+ * 
+ * how to create unsigned integer?
+ * - Some methods or operator treat them unsigned like >>>
+ * - Integer.parseUnsignedInt("xyz");, is another way in java-8
+ * 
  * */
 public class UnsignedRightShiftOperator {
 
 	public static void main(String[] args) {
-		int i = -25;
-		System.out.println(i>>3);
-		System.out.println(i>>>3);
+		int vInt = Integer.parseUnsignedInt("4294967295");
+		
+		System.out.println(vInt>>3);
+		System.out.println(vInt>>>3);
 	}
 }
