022e5e76bdb3fe0076373943d78cd359b4f529a4
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-19 15:00:50 +0530

    Add files via upload

--- UnsignedRightShiftOperator.java
+++ UnsignedRightShiftOperator.java
@@ -4,11 +4,13 @@ package com.project.java;
  * Unsigned right shift Operator >>> (does not respect sign of Number, does not preserve the 1st bit)
  * Why no unsigned left Operator[<<<]?
  * - As <<, serves the same purpose.
- * It does not preserve the sign bit [Left most]
- * 
+ * - It does not preserve the sign bit [Left most]
+ * - Holding of the property of signed or unsigned, is the related to signed or unsigned interger
+ * - Operator are not responsible for this at all
+ 
  * how to create unsigned integer?
  * - Some methods or operator treat them unsigned like >>>
- * - Integer.parseUnsignedInt("xyz");, is another way in java-8
+ * - Integer.parseUnsignedInt("xyz");, is another way in java-8 
  * 
  * */
 public class UnsignedRightShiftOperator {
