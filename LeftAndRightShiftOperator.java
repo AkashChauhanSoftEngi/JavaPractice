58382a25188e520d1dbc54ce92fbbc1977995fa9
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-19 14:19:17 +0530

    Add files via upload

---
+++ LeftAndRightShiftOperator.java
@@ -0,0 +1,16 @@
+package com.project.java;
+
+/*
+ * Left Shift	: N*(2^b)
+ * Right Shift	: N/(2^b)
+ * It preserves the sign bit [Left most]
+ * */
+
+public class LeftAndRightShiftOperator {
+
+	public static void main(String[] args) {
+		byte i = 127;
+		i=(byte) (i << 2);
+		System.out.println(i); 
+	}
+}
---
+++ UnsignedRightShiftOperator.java
@@ -0,0 +1,15 @@
+package com.project.java;
+
+/*
+ * Unsigned right shift Operator >>> (does not respect sign of Number, does not preserve the 1st bit)
+ * Why no unsigned left Operator?
+ * It does not preserve the sign bit [Left most]
+ * */
+public class UnsignedRightShiftOperator {
+
+	public static void main(String[] args) {
+		int i = -25;
+		System.out.println(i>>3);
+		System.out.println(i>>>3);
+	}
+}
