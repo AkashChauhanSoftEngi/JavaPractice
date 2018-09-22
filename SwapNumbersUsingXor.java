7614b3086b700fdba1515029a544deaeb7e6d600
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-22 12:32:33 +0530

    Add files via upload

---
+++ SwapNumbersUsingXor.java
@@ -0,0 +1,29 @@
+package com.project.java;
+
+/*
+ * If you XOR a number with itself it becomes 0 
+ * */
+
+public class SwapNumbersUsingXor {
+
+	public static void main(String[] args) {
+		int i=2,j=3;
+
+		int temp = i;
+		i = (i ^ j) ^i;
+		j = (temp ^ j) ^j;
+		
+		System.out.println(i + "," + j);
+		
+		i=2;
+		j=3;
+		
+		/*Without any temporary variable*/
+		j = i ^ j; // i = 2, j = ...
+		i = i ^ j; // i = 3, j = ...
+		j = j ^ i; // i = 3, j = 2
+		
+		System.out.println(i + "," + j);
+	}
+
+}
